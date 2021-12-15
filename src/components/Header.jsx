93148eed92cc50ea53683abaf2cc066cd0a79afd
Ismail Tarik Erkan <taricerkan@gmail.com>
2021-12-15 15:47:36 +0300

    basic components

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  return (
+    <div className="header">
+      <h1>{title}</h1>
+      <Button
+        color="green"
+        text="add"
+        onClick={() => {
+          console.log("Click");
+        }}
+      />
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,11 @@
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h1 key={task.id}>{task.text}</h1>
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
