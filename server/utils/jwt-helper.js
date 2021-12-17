a914b554f5178e8804d265e9710b271514ae3579
Ismail Tarik Erkan <taricerkan@gmail.com>
2021-12-17 20:12:51 +0300

    started writing middleware

--- server/utils/jwt-helper.js
+++ server/utils/jwt-helper.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 module.exports = function jwtTokens({ user_uuid }) {
   const user = { user_uuid };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "20s",
+    expiresIn: "1m",
   });
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "5m",
+    expiresIn: "15m",
   });
   return { accessToken, refreshToken };
 };
